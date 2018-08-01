ec1cd5c4723fc3a70169faa7062105d562e866a7
jace <jace.sf84@gmail.com>
2018-08-01 15:34:44 -0400

    trying to fix fs error

--- liri.js
+++ liri.js
@@ -117,10 +117,10 @@ switch(command){
             console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
             console.log("It's on Netflix!");
       
-            //adds text to log.txt
-            fs.appendFile('log.txt', "-----------------------");
-            fs.appendFile('log.txt', "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
-            fs.appendFile('log.txt', "It's on Netflix!");
+            // //adds text to log.txt
+            // fs.appendFile('log.txt', "-----------------------");
+            // fs.appendFile('log.txt', "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            // fs.appendFile('log.txt', "It's on Netflix!");
           }
         });
       

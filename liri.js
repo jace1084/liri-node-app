5fe37142fae15991e4e94d94ff0370b272634c87
jace <jace.sf84@gmail.com>
2018-08-01 13:13:36 -0400

    process.argv varibles and loop

--- liri.js
+++ liri.js
@@ -7,3 +7,16 @@ var request = require('request');
 var spotify = require('spotify');
 // var client = new twitter(keys.twitterKeys);
 var fs = require('fs');
+
+var nodeArgv = process.argv;
+var command = process.argv[2];
+//movie or song
+var x = "";
+//attaches multiple word arguments
+for (var i=3; i<nodeArgv.length; i++){
+  if(i>3 && i<nodeArgv.length){
+    x = x + "+" + nodeArgv[i];
+  } else{
+    x = x + nodeArgv[i];
+  }
+}
